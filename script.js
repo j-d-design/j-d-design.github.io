2251f0a14ee4ea3cc2eb2bf9cb5cfbebf85b8c25
j-d-design <domjaworski99@gmail.com>
2024-07-04 12:35:12 +0200

    commit

--- script.js
+++ script.js
@@ -1,14 +1,183 @@
-window.scroll({
-    top: 100,
-    left: 100,
-    behavior: "smooth",
-  });
+var imagesURL = ["photos/1.jpg", "photos/1-1.jpg", "photos/2.jpg", "photos/2-2.jpg", "photos/3.jpg", "photos/3-3.jpg", "photos/4.jpg", "photos/4-4.jpg", "photos/5.jpg", "photos/5-5.jpg", "photos/6.jpg", "photos/6-6.jpg", "photos/7.jpg", "photos/7-7.jpg", "photos/8.jpg", "photos/8-8.jpg", "photos/9.jpg", "photos/9-9.jpg", "photos/10.jpg", "photos/10-10.jpg"];
+var logosURL = ["logos/1.png", "logos/2.png", "logos/3.png"];
+var images = imagesURL.length;
+var logos = logosURL.length;
+let x = 0;
+let y = 0;
+let before = document.getElementById("before");
+let after = document.getElementById("after");
+before.src = imagesURL[2*x];
+after.src = imagesURL[2*x + 1];
+let logo = document.getElementById("logo");
+logo.src = logosURL[y];
 
-var menuButton = document.querySelector(".burger-menu");
-var menuList = document.querySelector(".navigation-list");
-
-menuButton.addEventListener('click', function(e)
+before.onload = function()
 {
-    e.preventDefault();
-    menuList.classList.toggle('show');
-});
+    let width = before.naturalWidth;
+    let height = before.naturalHeight;
+
+    if(width/height == 16 / 9)
+    before.style.aspectRatio = 16 / 9;
+    else
+    before.style.aspectRatio = 4 / 3;
+}
+
+after.onload = function()
+{
+    let width = after.naturalWidth;
+    let height = after.naturalHeight;
+
+    if(width/height == 16 / 9)
+    after.style.aspectRatio = 16 / 9;
+    else
+    after.style.aspectRatio = 4 / 3;
+}
+
+function nextPhoto()
+{
+    if((2*x)+1 < images-1)
+        {
+            x++;
+            before.src = imagesURL[2*x];
+            after.src = imagesURL[2*x + 1];
+
+            before.onload = function()
+            {
+                let width = before.naturalWidth;
+                let height = before.naturalHeight;
+
+                if(width/height == 16 / 9)
+                before.style.aspectRatio = 16 / 9;
+                else
+                before.style.aspectRatio = 4 / 3;
+            }
+
+            after.onload = function()
+            {
+                let width = after.naturalWidth;
+                let height = after.naturalHeight;
+
+                if(width/height == 16 / 9)
+                after.style.aspectRatio = 16 / 9;
+                else
+                after.style.aspectRatio = 4 / 3;
+            }
+        }
+    else
+    {
+        x=0;
+        before.src = imagesURL[2*x];
+        after.src = imagesURL[2*x + 1];
+
+        before.onload = function()
+        {
+            let width = before.naturalWidth;
+            let height = before.naturalHeight;
+
+            if(width/height == 16 / 9)
+            before.style.aspectRatio = 16 / 9;
+            else
+            before.style.aspectRatio = 4 / 3;
+        }
+
+        after.onload = function()
+        {
+            let width = after.naturalWidth;
+            let height = after.naturalHeight;
+
+            if(width/height == 16 / 9)
+            after.style.aspectRatio = 16 / 9;
+            else
+            after.style.aspectRatio = 4 / 3;
+        }
+    }
+}
+
+function previousPhoto()
+{
+    if((2*x)+1 > 2)
+        {
+            x--;
+            before.src = imagesURL[2*x];
+            after.src = imagesURL[2*x + 1];
+
+            before.onload = function()
+            {
+                let width = before.naturalWidth;
+                let height = before.naturalHeight;
+
+                if(width/height == 16 / 9)
+                before.style.aspectRatio = 16 / 9;
+                else
+                before.style.aspectRatio = 4 / 3;
+            }
+
+            after.onload = function()
+            {
+                let width = after.naturalWidth;
+                let height = after.naturalHeight;
+
+                if(width/height == 16 / 9)
+                after.style.aspectRatio = 16 / 9;
+                else
+                after.style.aspectRatio = 4 / 3;
+            }
+
+        }
+    else
+    {
+        x=(images/2) - 1;
+        before.src = imagesURL[2*x];
+        after.src = imagesURL[2*x + 1];
+
+        before.onload = function()
+        {
+            let width = before.naturalWidth;
+            let height = before.naturalHeight;
+
+            if(width/height == 16 / 9)
+            before.style.aspectRatio = 16 / 9;
+            else
+            before.style.aspectRatio = 4 / 3;
+        }
+
+        after.onload = function()
+        {
+            let width = after.naturalWidth;
+            let height = after.naturalHeight;
+
+            if(width/height == 16 / 9)
+            after.style.aspectRatio = 16 / 9;
+            else
+            after.style.aspectRatio = 4 / 3;
+        }
+    }
+}
+
+function nextLogo()
+{
+    if(y < logos-1)
+        {
+            y++;
+            logo.src = logosURL[y];
+        }
+    else
+    {
+        y=0;
+        logo.src = logosURL[y];
+    }
+}
+
+function previousLogo()
+{
+    if(y < 1)
+        {
+            y = logos-1;
+            logo.src = logosURL[y];
+        }
+    else
+    {
+        y--;
+        logo.src = logosURL[y];
+    }
+}
